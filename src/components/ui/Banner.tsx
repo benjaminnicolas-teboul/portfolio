6bc726de443a58799eb50a9f3042ca91dd94f5a2
benjaminnicolas-teboul <benjtenuts@hotmail.fr>
2025-08-13 10:59:35 +0200

    feat responive Banner

--- src/components/ui/Banner.tsx
+++ src/components/ui/Banner.tsx
@@ -14,35 +14,37 @@ const Banner = ({
   imageSrc,
   imageAlt = "banner image",
 }: BannerProps) => {
-  return (
-    <section className="w-full flex h-96 md:h-[600px] overflow-hidden shadow-md">
-      {/* Partie gauche : fond noir avec texte (40%) */}
-      <div className="overflow-hidden text-ellipsis w-2/5 bg-black flex flex-col justify-center p-6 md:p-12 text-white">
-        <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-4 break-words">
-          {titleSpans.map((text, index) => (
-            <span key={index} className="block">
-              {text}
-            </span>
-          ))}
-        </h2>
+ return (
+  <section className="w-full flex flex-col-reverse md:flex-row h-auto md:h-[600px] overflow-hidden shadow-md">
+    
+    {/* Partie gauche : fond noir avec texte */}
+    <div className="w-full md:w-2/5 bg-black flex flex-col justify-center p-6 md:p-12 text-white break-words">
+      <h2 className="text-3xl sm:text-4xl lg:text-6xl font-bold mb-4">
+        {titleSpans.map((text, index) => (
+          <span key={index} className="block">
+            {text}
+          </span>
+        ))}
+      </h2>
 
-        <p className="text-xl md:text-2xl">{subtitle}</p>
-      </div>
+      <p className="text-xl md:text-2xl">{subtitle}</p>
+    </div>
 
-      {/* Partie droite : image (60%) */}
-      <div className="w-3/5 relative">
-        <Image
-          src={imageSrc}
-          alt={imageAlt}
-          fill
-          quality={95}
-          className="object-cover"
-          sizes="(max-width: 768px) 60vw, 60vw"
-          priority
-        />
-      </div>
-    </section>
-  );
+    {/* Partie droite : image */}
+    <div className="w-full md:w-3/5 relative h-64 md:h-auto">
+      <Image
+        src={imageSrc}
+        alt={imageAlt}
+        fill
+        quality={95}
+        className="object-cover"
+        sizes="(max-width: 768px) 100vw, 60vw"
+        priority
+      />
+    </div>
+
+  </section>
+);
 };
 
 export default Banner;
